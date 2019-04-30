321627ef1dfff3bf54cb07a3bc4d192c7dd11c63
Francis Mendez <fmendez884@gmail.com>
2019-04-30 21:33:27 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,13 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |season|
+    season[1].each do |person|
+        if person[:occupation] == occupation
+          return person[:name]
+        end 
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
